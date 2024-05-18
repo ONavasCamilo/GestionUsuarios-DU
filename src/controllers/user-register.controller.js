9f8bcff17106a7a4fb78647130761422fe7306b7
camilo Navas <navasochoa@hotmail.com>
2024-05-18 11:37:03 -0500

    _id: { type: String, _id: false },

--- src/controllers/user-register.controller.js
+++ src/controllers/user-register.controller.js
@@ -3,27 +3,31 @@ import { hash } from 'bcrypt';
 
 const userRegisterController = async (req, res) => {
     const { _id, name, surname, email, password } = req.body;
-    const existingUserById = await UserModel.findById(_id).exec();
-    if (existingUserById)
-        return res.status(409).send({
-            errors: ['Ya existe un usuario con ese id registrado'],
-        });
-    const existingUserByEmail = await UserModel.findOne({ email }).exec();
-    if (existingUserByEmail)
-        return res.status(409).send({
-            errors: ['Ya existe un usuario con ese email registrado'],
-        }); 
+    try {
+        const existingUserById = await UserModel.findById(_id).exec();
+        if (existingUserById)
+            return res.status(409).send({
+                errors: ['Ya existe un usuario con ese id registrado'],
+            });
+        const existingUserByEmail = await UserModel.findOne({ email }).exec();
+        if (existingUserByEmail)
+            return res.status(409).send({
+                errors: ['Ya existe un usuario con ese email registrado'],
+            });
 
-    const hashedPassword = await hash(password, 10);
-    const user = new UserModel({
-        _id,
-        name,
-        surname,
-        email,
-        password: hashedPassword,
-    });
-    await user.save();
-    return res.send('usuario registrado con exito');
+        const hashedPassword = await hash(password, 5);
+        const user = new UserModel({
+            _id,
+            name,
+            surname,
+            email,
+            password: hashedPassword,
+        });
+        await user.save();
+        return res.send('usuario registrado con exito');
+    } catch (err) {
+        res.status(500).send({ err });
+    }
 };
 
 export default userRegisterController;
